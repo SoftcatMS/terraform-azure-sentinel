9769494b358bfdff957291eb33235d4755558747
kevinbasoftcat <kevinba@softcat.com>
2022-09-08 11:44:59 +0100

    Adding test configuration for Azure DevOps

--- tests/advanced/provider.tf
+++ tests/advanced/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">=1.0.0"
 
-#Azurerm Backend https://www.terraform.io/docs/language/settings/backends/azurerm.html#
-# backend "azurerm" {
-#     resource_group_name  = "rg-terraform-modules-state"
-#     storage_account_name = "softcatmodulestate"
-#     container_name       = "tf-modules-azure-sentinel-advanced"
-#     key                  = "terraform.tfstate"  
-#   }
+# Azurerm Backend https://www.terraform.io/docs/language/settings/backends/azurerm.html#
+backend "azurerm" {
+    resource_group_name  = "rg-terraform-modules-state"
+    storage_account_name = "softcatmodulestate"
+    container_name       = "tf-modules-azure-sentinel-advanced"
+    key                  = "terraform.tfstate"  
+  }
 
 # Uncomment required providers and delete unneeded
 required_providers {
--- tests/basic/provider.tf
+++ tests/basic/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=1.0.0"
 
-#Azurerm Backend https://www.terraform.io/docs/language/settings/backends/azurerm.html#
+# Azurerm Backend https://www.terraform.io/docs/language/settings/backends/azurerm.html#
 backend "azurerm" {
     resource_group_name  = "rg-terraform-modules-state"
     storage_account_name = "softcatmodulestate"
