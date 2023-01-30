4a123508f094141a7e6979c23706950763ccced1
kevinbasoftcat <kevinba@softcat.com>
2023-01-30 13:31:23 +0000

    Adjusting workspace ID output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "log_analytics_workspace_id" {
-  value       = azurerm_log_analytics_workspace.sentinel.id
+  value       = azurerm_log_analytics_workspace.sentinel.workspace_id
   description = "The log analytics workspace id that is created and setup for Sentinel"
 }
 
